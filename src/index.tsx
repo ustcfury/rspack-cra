7a6b72d9fe80438d69c829ddf91a0b8bf6d1acd1
绍晗 <zhaoshaoting.zst@alibaba-inc.com>
2025-03-04 16:46:19 +0800

    feat: init

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,22 @@ import App from "./App";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-if ((module as any).hot) {
-  (module as any).hot.accept("./App", () => {
-    root.render(<React.StrictMode>123</React.StrictMode>);
-  });
-}
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// if ((module as any).hot) {
+//   (module as any).hot.accept("./App", () => {
+//     root.render(
+//       <React.StrictMode>
+//         <App />
+//       </React.StrictMode>
+//     );
+//   });
+// }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
